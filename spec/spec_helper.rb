7e22c3a31b9acbc9a4398c9aa40e50ca6cb3dd61
marcotsui2003 <marcotsui2003@yahoo.com>
2016-02-03 12:00:05 -0500

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,18 @@
+# Load RSpec and Capybara
+require 'rspec'
 require 'capybara/rspec'
 require 'capybara/dsl'
 
+# Configure RSpec
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.order = 'default'
 end
 
+# Define the application we're testing
 def app
   Rack::Builder.parse_file('config.ru').first
 end
 
+# Configure Capybara to test against the application above.
 Capybara.app = app

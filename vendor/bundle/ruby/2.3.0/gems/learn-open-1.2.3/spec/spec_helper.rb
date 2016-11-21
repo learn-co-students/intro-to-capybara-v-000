363f9e118e4c0dc4f46e4794f523df1f240d4dc3
Chenxi Wu <wuchenxi2013@gmail.com>
2016-11-21 05:52:07 +0100

    Done.

---
+++ vendor/bundle/ruby/2.3.0/gems/learn-open-1.2.3/spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require_relative '../lib/learn_open'
---
+++ vendor/bundle/ruby/2.3.0/gems/learn-web-1.5.0/lib/learn_web/client.rb
@@ -0,0 +1,40 @@
+require 'learn_web/client/request'
+require 'learn_web/client/connection'
+require 'learn_web/client/user'
+require 'learn_web/client/pull_request'
+require 'learn_web/client/lesson'
+require 'learn_web/client/validate_repo'
+require 'learn_web/client/fork'
+require 'learn_web/client/environment'
+require 'learn_web/client/event'
+
+module LearnWeb
+  class Client
+    attr_reader :token, :conn, :silent_output
+
+    LEARN_URL = 'https://learn.co'
+    API_ROOT  = '/api/v1'
+
+    include LearnWeb::Client::Request
+    include LearnWeb::Client::Connection
+    include LearnWeb::Client::PullRequest
+    include LearnWeb::Client::Lesson
+    include LearnWeb::Client::ValidateRepo
+    include LearnWeb::Client::Fork
+    include LearnWeb::Client::User
+    include LearnWeb::Client::Environment
+    include LearnWeb::Client::Event
+
+    def initialize(token:, silent_output: false)
+      @token = token
+      @silent_output = silent_output
+      @conn = Faraday.new(url: LEARN_URL) do |faraday|
+        faraday.adapter Faraday.default_adapter
+      end
+    end
+
+    def valid_token?
+      !!me.data
+    end
+  end
+end

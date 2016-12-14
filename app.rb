24fab909389b61cbea36e5b17359c26e6241a4ba
Pia Kim <pkim2320@gmail.com>
2016-12-14 06:43:19 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 class Application < Sinatra::Base
   # write your code here!
+  get '/' do
+    erb :index
+  end
+
+  # New route to respond to the form submission
+post '/greet' do
+  erb :greet
 end
 
+end
+
+
+# The line erb :index tells the application to render, or deliver to the user's browser, the file in views/index.erb. This is a Sinatra-provided functionality that renders ERB templates located in the views directory.

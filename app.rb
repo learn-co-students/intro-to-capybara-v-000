273501276b1bfa05a265499352b5f974d9e4086c
Scott Young <young.scottw@gmail.com>
2017-01-24 16:05:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
+##Adding comment to allow submition :)
 class Application < Sinatra::Base
-  get '/' do		
-    erb :index		
-  end		
-  
-  post '/greet' do		
-    erb :greet		
+  get '/' do
+    erb :index
+  end
+
+  post '/greet' do
+    erb :greet
   end
 end
-

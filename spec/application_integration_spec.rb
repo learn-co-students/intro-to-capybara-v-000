ba42ed2ade7b1b29a307098d394ca00a0e7a1515
bortch89 <bortch@gmail.com>
2017-03-30 15:38:12 +0200

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -4,14 +4,14 @@ describe "GET '/' - Greeting Form" do
   # Code from previous example
   it 'welcomes the user' do
     visit '/'
-    expect(page.body).to include("Welcome!")
+    expect(page.body).to include('Welcome!')
   end
 
   # New test
   it 'has a greeting form with a user_name field' do
     visit '/'
 
-    expect(page).to have_selector("form")
+    expect(page).to have_selector('form')
     expect(page).to have_field(:user_name)
   end
 end
@@ -20,9 +20,34 @@ describe "POST '/greet' - User Greeting" do
   it 'greets the user personally based on their user_name in the form' do
     visit '/'
 
-    fill_in(:user_name, :with => "Avi")
-    click_button "Submit"
+    fill_in(:user_name, with: 'Avi')
+    click_button 'Submit'
 
-    expect(page).to have_text("Hi Avi, nice to meet you!")
+    expect(page).to have_text('Hi Avi, nice to meet you!')
+  end
+
+  describe "GET '/' - Greeting Form" do
+    it 'welcomes the user' do
+      visit '/'
+      expect(page.body).to include('Welcome!')
+    end
+
+    it 'has a greeting form with a user_name field' do
+      visit '/'
+
+      expect(page).to have_selector('form')
+      expect(page).to have_field(:user_name)
+    end
+  end
+
+  describe "POST '/greet' - User Greeting" do
+    it 'greets the user personally based on their user_name in the form' do
+      visit '/'
+
+      fill_in(:user_name, with: 'Avi')
+      click_button 'Submit'
+
+      expect(page).to have_text('Hi Avi, nice to meet you!')
+    end
   end
 end

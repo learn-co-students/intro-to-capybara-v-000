915e2356bacda523fc64d8458bc7b32a39023cf1
Sergio Mendoza <sergiomendoza@Sergios-MacBook-Pro.local>
2017-04-28 21:07:36 -0700

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe "GET '/' - Greeting Form" do
-  # Code from previous example
   it 'welcomes the user' do
     visit '/'
     expect(page.body).to include("Welcome!")

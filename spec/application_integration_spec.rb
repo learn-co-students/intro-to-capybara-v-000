859ce05144a6e5748aecaf9d465f6d13e7e9ee2c
Nadine Fischoff <nadine.khattak@gmail.com>
2017-04-30 10:22:44 +0000

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,23 +1,21 @@
 require 'spec_helper'
-
+ 
 describe "GET '/' - Greeting Form" do
-  # Code from previous example
   it 'welcomes the user' do
     visit '/'
     expect(page.body).to include("Welcome!")
   end
 
-  # New test
-  it 'has a greeting form with a user_name field' do
-    visit '/'
 
-    expect(page).to have_selector("form")
-    expect(page).to have_field(:user_name)
-  end
+it 'has a greeting form with a user_name field' do 
+  visit '/'
+
+  expect(page).to have_selector("form")
+  expect(page).to have_field(:user_name)
 end
 
-describe "POST '/greet' - User Greeting" do
-  it 'greets the user personally based on their user_name in the form' do
+describe "Post '/greet' - User Greeting" do 
+  it 'greets the user personally based on their user_name in the form' do 
     visit '/'
 
     fill_in(:user_name, :with => "Avi")
@@ -25,4 +23,5 @@ describe "POST '/greet' - User Greeting" do
 
     expect(page).to have_text("Hi Avi, nice to meet you!")
   end
+end
 end

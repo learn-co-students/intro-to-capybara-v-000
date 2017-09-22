7fd0a33447820b451f1260830e76893fc3591cbf
Donna <donnamcla@gmail.com>
2017-09-22 15:58:56 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index
+  end
 
+  # this  route responds to the form submission
+  post '/greet' do
+    erb :greet
+  end
 end

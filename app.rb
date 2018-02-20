601c90e1b16ea4c572be32ba399d91d2b310eec0
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2018-02-20 19:27:38 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
-  # Write your code here!
-
+#==================routes================== 
+  # index
+  get "/" do
+  	erb :index
+  end
+  
+  post "/greet" do
+  	erb :greet
+  end 
+#========================================== 
 end

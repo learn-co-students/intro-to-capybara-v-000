796bf4e1afa46218fabdc3d38af7fd1ed6d2d1af
eyi1 <ej7729@gmail.com>
2018-05-29 14:16:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
-class Application < Sinatra::Base
-  # Write your code here!
+class Application < Sinatra::Base #transforms standard ruby class into sinatra controller
+ 
+  get '/' do
+    erb :index #tells the application to render/deliver to user's browswer, the file in views/index.erb; Sinatra-provided functionality that renders ERB templates
+  end
 
+  post '/greet' do 
+    erb :greet
+  end
 end
--- config.ru
+++ config.ru
@@ -1,1 +1,7 @@
-# Write your code here!
+require 'sinatra'
+
+require_relative './app'
+
+run Application
+
+# puts everything together and starts our Sinatra application

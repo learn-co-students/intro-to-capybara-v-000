493c37cf8ca348dac7c2dc048f4214798df4db57
Pamela Frieze <pfrieze@gmail.com>
2018-06-26 18:03:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
-class Application < Sinatra::Base
+class Application < Sinatra::Base #controller to power web app
   # Write your code here!
+  get '/' do
+    erb :index
+  end
+
+  # New route to respond to the form submission
+  post '/greet' do
+    erb :greet
+  end
 
 end

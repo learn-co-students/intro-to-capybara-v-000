217db5d4cea8e4058386a31fc08a5caeee90e254
Micah Shute <micah.shute@gmail.com>
2018-08-09 22:17:45 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  
+  get '/' do
+    erb :index
+  end
+
+  post '/greet' do
+    @user = params[:user_name]
+    erb :greet
+  end
 
 end

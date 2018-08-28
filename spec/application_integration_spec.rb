21a146841a1038356ceaf464291ec001a0ea92cd
github username <github email address>
2018-08-28 13:08:21 +0200

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,28 +1,20 @@
-require 'spec_helper'
-
-describe "GET '/' - Greeting Form" do
-  # Code from previous example
-  it 'welcomes the user' do
-    visit '/'
-    expect(page.body).to include("Welcome!")
-  end
-
-  # New test
-  it 'has a greeting form with a user_name field' do
-    visit '/'
-
-    expect(page).to have_selector("form")
-    expect(page).to have_field(:user_name)
-  end
+# Load RSpec and Capybara
+require 'rspec'
+require 'capybara/rspec'
+require 'capybara/dsl'
+ 
+# Configure RSpec
+RSpec.configure do |config|
+  # Mixin the Capybara functionality into Rspec
+  config.include Capybara::DSL
+  config.order = 'default'
 end
-
-describe "POST '/greet' - User Greeting" do
-  it 'greets the user personally based on their user_name in the form' do
-    visit '/'
-
-    fill_in(:user_name, :with => "Avi")
-    click_button "Submit"
-
-    expect(page).to have_text("Hi Avi, nice to meet you!")
-  end
+ 
+# Define the application we're testing
+def app
+  # Load the application defined in config.ru
+  Rack::Builder.parse_file('config.ru').first
 end
+ 
+# Configure Capybara to test against the application above.
+Capybara.app = app

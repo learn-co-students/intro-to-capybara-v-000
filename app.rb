284a3d336773462353df90203d621893890fd4d4
Madeline Stark <madeline.stark1@gmail.com>
2018-09-10 16:21:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
-  # Write your code here!
+    get '/' do
+      erb :index
+    end
+
+    #for now only repsonds to /, write below to respond to /greet
+
+    post '/greet' do
+      erb :greet
+    end
 
 end

183b8d95472fa9bb866c007678e86da0d23a303e
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-05-27 11:33:36 -0700

    Completeed lab

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index
+  end
 
+  post '/greet' do
+    #raise params.inspect
+    @user_name = params["user_name"]
+
+    erb :greet
+  end
 end
